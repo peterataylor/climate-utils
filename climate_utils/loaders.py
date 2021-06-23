b7fa4cce9596bfc7b0334421a57cd9d3da5c91de
Joel Rahman <joel@flowmatters.com.au>
2021-06-24 09:34:33 +1000

    Refactored netcdf loader for consistency

--- climate_utils/loaders.py
+++ climate_utils/loaders.py
@@ -54,11 +54,50 @@ def awap_ascii_by_year(base_path,fn_pattern,date_format):
     return loader
 
 def netcdf_loader(fn_pattern,known_bounds=None):
+    import netCDF4 as nc
     fn_template = _as_template(fn_pattern)
 
     if known_bounds is not None:
-        return bounded_netcdf_loader(fn_template,known_bounds)
-    return whole_grid_netcdf_loader(fn_template)
+        if hasattr(known_bounds,'total_bounds'):
+            known_bounds = known_bounds.total_bounds
+        min_lon,min_lat,max_lon,max_lat = known_bounds
+        lon_range = [min_lon,max_lon]
+        lat_range = [min_lat,max_lat]
+        find_slice = _find_slice
+    else:
+        lat_range = None
+        lon_range = None
+
+        def find_slice(*args):
+            return slice(None)
+
+    def loader(variable,date):
+        args = _pattern_substitutions(variable,date)
+        fn = fn_template.substitute(**args)
+
+        dataset = nc.Dataset(fn,'r')
+        try:
+            x_var = _x_var(dataset)
+            y_var = _y_var(dataset)
+
+            x_var.set_auto_mask(False)
+            y_var.set_auto_mask(False)
+
+            x_slice = find_slice(lon_range,x_var)
+            y_slice = find_slice(lat_range,y_var)
+
+            time_var = _time_var(dataset)
+            if time_var is None:
+                arr = dataset.variables[variable][y_slice,x_slice]
+            else:
+                ix = _nc_date_index(date,time_var)
+                arr = dataset.variables[variable][ix,y_slice,x_slice]
+            affine = _affine_from_nc(x_var[x_slice],y_var[y_slice])
+
+            return arr,affine
+        finally:
+            dataset.close()
+    return loader
 
 def _first_var(dataset,names):
     for v in names:
@@ -80,101 +119,116 @@ def _find_slice(dim_range,dimension_variable):
     if (max(dim_range) > max(data)) or (min(dim_range) < min(data)):
         raise Exception('Out of bounds. Dimension %s does not cover required range'%dimension_variable.name)
 
-    if data[0] > data[-1]:
+    if data[0] > data[-1]: # Decreasing - ie N-S or E-W orientation
         start = np.argmin(dim_range[0]<data)
         end = np.argmax(data<dim_range[1]) -1
         if data[end] > dim_range[1]:
             end -= 1
         step = -1
-    else:
+    else: # Increasing - ie S-N or W-E orientation
         start = np.argmax(data>dim_range[0])
         if data[start] > dim_range[0]:
             start -= 1
         step = 1
 
         end = np.argmin(dim_range[1]>data)+1
-            
-    return (start,end,step) 
 
-def _affine_dim(v,slice):
-    size = abs(v[1]-v[0])
-    return size,slice[0],slice[2]
+    if start < 0:
+        start = None
+    if end < 0:
+        end = None
+    return slice(start,end,step) 
 
-def _affine_from_nc(x_var,x_slice,y_var,y_slice):
+def _affine_dim(v):
+    size = (v[-1]-v[0])/(v.shape[0]-1)
+    return size,v[0]-size/2.0
+
+def _affine_from_nc(x_var,y_var):
     from affine import Affine
-    x_size, x_0, x_step = _affine_dim(x_var,x_slice)
-    y_size, y_0, y_step = _affine_dim(y_var,y_slice)
+    x_size, x_0 = _affine_dim(x_var)
+    y_size, y_0 = _affine_dim(y_var)
 
-    return Affine(x_step*x_size,0,np.array(x_var)[x_0],
-                  0,y_step*y_size,np.array(y_var)[y_0])
+    return Affine(x_size,0,x_0,
+                  0,y_size,y_0)
 
-
-def bounded_netcdf_loader(fn_template,known_bounds):
+def _nc_date_index(date,nc_time_var):
     import netCDF4 as nc
-    if hasattr(known_bounds,'total_bounds'):
-        known_bounds = known_bounds.total_bounds
-    min_lon,min_lat,max_lon,max_lat = known_bounds
-    lon_range = [min_lon,max_lon]
-    lat_range = [min_lat,max_lat]
+    if hasattr(date,'to_pydatetime'):
+        date = date.to_pydatetime()
+    if hasattr(date,'to_datetime'):
+        date = date.to_datetime()
 
-    def loader(variable,date):
-        args = _pattern_substitutions(variable,date)
-        fn = fn_template.substitute(**args)
+    return nc.date2index(date,nc_time_var,select='exact')
 
-        dataset = nc.Dataset(fn,'r')
-        try:
-            if hasattr(date,'to_pydatetime'):
-                date = date.to_pydatetime()
-            if hasattr(date,'to_datetime'):
-                date = date.to_datetime()
+# def bounded_netcdf_loader(fn_template,known_bounds):
+#     import netCDF4 as nc
+#     if hasattr(known_bounds,'total_bounds'):
+#         known_bounds = known_bounds.total_bounds
+#     min_lon,min_lat,max_lon,max_lat = known_bounds
+#     lon_range = [min_lon,max_lon]
+#     lat_range = [min_lat,max_lat]
 
-            ix = nc.date2index(date,_time_var(dataset),select='exact')
-            x_var = _x_var(dataset)
-            y_var = _y_var(dataset)
+#     def loader(variable,date):
+#         args = _pattern_substitutions(variable,date)
+#         fn = fn_template.substitute(**args)
 
-            x_slice = _find_slice(lon_range,x_var)
-            y_slice = _find_slice(lat_range,y_var)
+#         dataset = nc.Dataset(fn,'r')
+#         try:
+#             x_var = _x_var(dataset)
+#             y_var = _y_var(dataset)
 
-            y_slice = (y_slice[1]-1,y_slice[0]-1,-y_slice[2])
-            if y_slice[1] < 0: y_slice[1] = None
-            if y_slice[0] < 0: y_slice[0] = None
+#             x_var.set_auto_mask(False)
+#             y_var.set_auto_mask(False)
 
-            arr = dataset.variables[variable][ix,slice(*y_slice),slice(*x_slice)] 
-            affine = _affine_from_nc(x_var,x_slice,y_var,y_slice)
+#             x_slice = _find_slice(lon_range,x_var)
+#             y_slice = _find_slice(lat_range,y_var)
 
-            return arr,affine
-        finally:
-            dataset.close()
-    return loader
+#             # y_slice = (y_slice[1]-1,y_slice[0]-1,-y_slice[2])
+#             if y_slice[1] < 0: y_slice[1] = None
+#             if y_slice[0] < 0: y_slice[0] = None
 
-def whole_grid_netcdf_loader(fn_template):
-    import netCDF4 as nc
+#             x_slice = slice(*x_slice)
+#             y_slice = slice(*y_slice)
+#             time_var = _time_var(dataset)
+#             if time_var is None:
+#                 arr = dataset.variables[variable][y_slice,x_slice] 
+#             else:
+#                 ix = _nc_date_index(date,time_var)
+#                 arr = dataset.variables[variable][ix,y_slice,x_slice] 
+#             affine = _affine_from_nc(x_var[x_slice],y_var[y_slice])
 
-    def loader(variable,date):
-        args = _pattern_substitutions(variable,date)
+#             return arr,affine
+#         finally:
+#             dataset.close()
+#     return loader
 
-        fn = fn_template.substitute(**args)
+# def whole_grid_netcdf_loader(fn_template):
+#     import netCDF4 as nc
 
-        dataset = nc.Dataset(fn,'r')
-        try:
-            x_var = _x_var(dataset)
-            y_var = _y_var(dataset)
+#     def loader(variable,date):
+#         args = _pattern_substitutions(variable,date)
+#         fn = fn_template.substitute(**args)
 
-            x_var.set_auto_mask(False)
-            y_var.set_auto_mask(False)
+#         dataset = nc.Dataset(fn,'r')
+#         try:
+#             x_var = _x_var(dataset)
+#             y_var = _y_var(dataset)
 
-            x_slice = _find_slice(sorted(x_var[...][[0,-1]]),x_var)
-            y_slice = _find_slice(sorted(y_var[...][[0,-1]]),y_var)
+#             x_var.set_auto_mask(False)
+#             y_var.set_auto_mask(False)
 
-            time_var = _time_var(dataset)
-            if time_var is None:
-                arr = dataset.variables[variable][slice(*y_slice),slice(*x_slice)] 
-            else:
-                ix = nc.date2index(date,time_var,select='exact')
-                arr = dataset.variables[variable][ix,slice(*y_slice),slice(*x_slice)] 
-            affine = _affine_from_nc(x_var,x_slice,y_var,y_slice)
-            return arr,affine
-        finally:
-            dataset.close()
-    return loader
+#             x_slice = slice(None)
+#             y_slice = slice(None)
+#             time_var = _time_var(dataset)
+#             if time_var is None:
+#                 arr = dataset.variables[variable][y_slice,x_slice]
+#             else:
+#                 ix = _nc_date_index(date,time_var)
+#                 arr = dataset.variables[variable][ix,y_slice,x_slice]
+#             affine = _affine_from_nc(x_var[x_slice],y_var[y_slice])
+
+#             return arr,affine
+#         finally:
+#             dataset.close()
+#     return loader
 
